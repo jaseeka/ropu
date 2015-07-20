2b1a86eb4bb9d22ac812fa8090bdcf8092563089
jaseeka <jaseeka@163.com>
2015-07-20 14:55:27 +0800

    提交

---
+++ src/main/java/com/ropu/service/IAdminService.java
@@ -0,0 +1,9 @@
+package com.ropu.service;
+
+/**
+ * Created by jaseeka
+ * date 2015/7/20
+ * time 14:53
+ */
+public interface IAdminService {
+}
---
+++ src/main/java/com/ropu/service/impl/AdminService.java
@@ -0,0 +1,11 @@
+package com.ropu.service.impl;
+
+import com.ropu.service.IAdminService;
+
+/**
+ * Created by jaseeka
+ * date 2015/7/20
+ * time 14:53
+ */
+public class AdminService implements IAdminService {
+}
